3788ce4d97b131588bd5ab02b2c2d82da5e12051
Devin Wallace <devinwallace@protonmail.com>
2019-04-10 01:15:14 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='MultiBinary',
+    version='1.0.0',
+    packages=[''],
+    url='https://github.com/Devin-Wallace/MultiBinary',
+    license='MIT License',
+    author='Devin',
+    author_email='devinwallace@protonmail.com',
+    description='A Multi-Thread Binary search written in Python3'
+)
